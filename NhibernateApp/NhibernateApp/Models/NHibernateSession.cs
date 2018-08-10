aa25f4b85b61773c0cf4511ca55e246df0c0f5d1
Paweł Fluder <pawelfluder@gmail.com>
2018-08-10 23:53:07 +0200

    Nhiberate project is created

---
+++ NhibernateApp/NhibernateApp/Models/NHibernateSession.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace NhibernateApp.Models
+{
+    public class NHibernateSession
+    {
+        public static ISession OpenSession()
+        {
+            var configuration = new Configuration();
+            var configurationPath = HttpContext.Current.Server.MapPath(@"~\hibernate.cfg.xml");
+            configuration.Configure(configurationPath);
+            var configurationFile = HttpContext.Current.Server.MapPath(@"~\Models\Person.hbm.xml");
+            configuration.AddFile(configurationFile);
+            ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+            return sessionFactory.OpenSession();
+        }
+    }
+}
